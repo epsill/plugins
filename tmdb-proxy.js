ce6f8377fa40e3c5fc48218148046b0d186208f1
epsill <164032791+epsill@users.noreply.github.com>
2024-04-14 22:14:29 +0500

    Add files via upload

---
+++ tmdb-proxy.js
@@ -0,0 +1,29 @@
+(function () {
+    'use strict';
+
+    var tmdb_proxy = {
+      name: 'TMDB Proxy',
+      version: '1.0.5',
+      description: 'Проксирование постеров и API сайта TMDB',
+      path_image: 'imagetmdb.com/',
+      path_api: 'tmapi.' + (Lampa.Manifest && Lampa.Manifest.cub_domain ? Lampa.Manifest.cub_domain : 'cub.red') + '/3/'
+    };
+
+    Lampa.TMDB.image = function (url) {
+      var base = Lampa.Utils.protocol() + 'image.tmdb.org/' + url;
+      return Lampa.Storage.field('proxy_tmdb') ? Lampa.Utils.protocol() + tmdb_proxy.path_image + url : base;
+    };
+
+    Lampa.TMDB.api = function (url) {
+      var base = Lampa.Utils.protocol() + 'api.themoviedb.org/3/' + url;
+      return Lampa.Storage.field('proxy_tmdb') ? Lampa.Utils.protocol() + tmdb_proxy.path_api + url : base;
+    };
+
+    Lampa.Settings.listener.follow('open', function (e) {
+      if (e.name == 'tmdb') {
+        e.body.find('[data-parent="proxy"]').remove();
+      }
+    });
+    console.log('TMDB-Proxy', 'started, enabled:', Lampa.Storage.field('proxy_tmdb'));
+
+})();
