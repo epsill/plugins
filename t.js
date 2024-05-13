9624aa531021989e99ca75fd66b6df2bd216668f
epsill <164032791+epsill@users.noreply.github.com>
2024-05-14 00:34:47 +0500

    Update t.js

--- t.js
+++ t.js
@@ -2,6 +2,6 @@ Lampa.Listener.follow('full', function(e) {
       if (e.type == 'complite') {
        setTimeout(function(){
 $('.view--torrent').insertAfter($('.button--play').last());
-       },10);
+       },1);
      }
   })
