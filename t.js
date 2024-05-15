b40e730e9148573d1b463bdda1a13062f1dcc6c5
epsill <164032791+epsill@users.noreply.github.com>
2024-05-15 05:33:45 +0500

    Update t.js

--- t.js
+++ t.js
@@ -1,7 +1,15 @@
 Lampa.Listener.follow('full', function(e) {
-      if (e.type == 'complite') {
-       setTimeout(function(){
-$('.view--torrent').insertAfter($('.button--play').prepend());
-       },10);
-     }
-  })
+  if (e.type == 'complite') {
+    setTimeout(function() {
+      $('.view--torrent').each(function() {
+        var $torrentButton = $(this);
+        var $parent = $torrentButton.parent();
+        var $firstChild = $parent.children().first();
+
+        if (!$torrentButton.is($firstChild)) {
+          $torrentButton.prependTo($parent);
+        }
+      });
+    }, 10);
+  }
+});
