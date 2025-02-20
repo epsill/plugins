4dd88669708f50a5ca5f201a0c0f1f6917ab7778
epsill <164032791+epsill@users.noreply.github.com>
2025-02-20 14:25:01 +0500

    Delete notr.js

--- notr.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    'use strict';	
-    Lampa.Listener.follow('full', function (e) {
-        if (e.type == 'complite') {
-            e.object.activity.render().find('.view--trailer').remove();
-        }
-    });
-})();
