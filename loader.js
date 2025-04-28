4f0cb4c83e4db1494e0c1d8fec887a736c8c90a7
epsill <164032791+epsill@users.noreply.github.com>
2025-04-28 14:13:41 +0500

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,112 +1,76 @@
+(function() {
+    'use strict';
 
-Lampa.Platform.tv();
+    Lampa.Platform.tv();
 
-var lampa_url     = 'http://185.105.117.217:12160';
-var cache_version = Math.floor((new Date()).getTime() / 9e5); // Новое значение каждые 15 минут
-    
-function urlJoin(base, add) {
-    if (base.charAt(base.length - 1) !== '/') {
-        base += '/'
+    const lampa_url = 'http://185.105.117.217:12160';
+    const cache_version = Math.floor((new Date()).getTime() / 9e5); // Новое значение каждые 15 минут
+
+    function urlJoin(base, add) {
+        return base.endsWith('/') ? base + add : base + '/' + add;
     }
 
-    return base + add
-}
+    function putScript(src, onload, onerror) {
+        console.log('Loader', 'try-load-script', src);
+        let tries = 0;
 
-function putScript(scripts, onload, onerror){
-    var add = function(src){
-        console.log('Loader', 'try-load-script', src)
+        const createScript = () => {
+            const script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.src = src;
 
-        var trys = 0
-
-        var sreateScript = function(){
-            var script = document.createElement('script')
-
-            script.type = 'text/javascript'
-
-            script.onload = script.onreadystatechange = function(e) {
-                console.log('Loader', 'Script loaded', e)
-
-                if (!!onload) onload()
+            script.onload = () => {
+                console.log('Loader', 'Script loaded', src);
+                if (onload) onload();
             };
 
-            script.onerror = function(){
-                trys++
-
-                console.log('Loader', 'error-load-script', src)
-
-                if(trys < 3){
-                    sreateScript()
-                } else if (!!onerror) {
-                    onerror()
-                } else{
-                    $('.no-network').removeClass('hide')
-
-                    $('.no-network__file').text(window.location.href + '/' + src)
+            script.onerror = () => {
+                tries++;
+                console.log('Loader', 'error-load-script', src);
+                if (tries < 3) {
+                    createScript(); // Повторная попытка загрузки
+                } else if (onerror) {
+                    onerror();
+                } else {
+                    $('.no-network').removeClass('hide');
+                    $('.no-network__file').text(window.location.href + '/' + src);
                 }
-            }
+            };
 
-            script.src = src
+            document.body.appendChild(script);
+        };
 
-            document.getElementsByTagName('body')[0].appendChild(script)
-        }
-
-        sreateScript()
+        createScript();
     }
 
-    add(scripts)
-}
+    function loadCurrentLampa() {
+        putScript(`app.min.js?v=${cache_version}`, () => {});
+    }
 
-function loadCurrentLampa() {
-    putScript('app.min.js?v=' + cache_version, function () {})
-}
+    function loadLocalLampa() {
+        if (lampa_url !== '') window.local_lampa = true;
+        loadCurrentLampa();
+    }
 
-function loadLocalLampa(){
-    if(lampa_url !== '') window.local_lampa = true
+    // Проверка на наличие AndroidJS
+    if (typeof AndroidJS !== "undefined" && !!AndroidJS.getLampaURL && /^https?:\/\//i.test(AndroidJS.getLampaURL()) && !/^https?:\/\//i.test(location.href)) {
+        lampa_url = urlJoin(AndroidJS.getLampaURL(), '');
+    }
 
-    loadCurrentLampa()
-}
+    if (lampa_url !== '') {
+        putScript(urlJoin(lampa_url, `lampainit.js?v=${cache_version}`), false, () => {});
 
-if (typeof AndroidJS !== "undefined"
-    && !!AndroidJS.getLampaURL
-    && /^https?:\/\//i.test(AndroidJS.getLampaURL())
-    && !/^https?:\/\//i.test(location.href)
-) {
-    lampa_url = urlJoin(AndroidJS.getLampaURL(),'')
-}
-
-if (lampa_url !== '') {
-    putScript(
-        urlJoin(lampa_url, 'lampainit.js?v=' + cache_version),
-        false,
-        function(){}
-    )
-
-    putScript(
-        urlJoin(lampa_url, 'app.min.js?v=' + cache_version),
-        function () {
-            var css = urlJoin(lampa_url, 'css/app.css?v=' + cache_version)
-
-            $('head').append('<link rel="stylesheet" href="' + css + '">')
-        },
-        function () {
-            putScript(
-                urlJoin(lampa_url, 'lampa-main/app.min.js?v=' + cache_version),
-                function () {
-                    var css = urlJoin(lampa_url, 'lampa-main/css/app.css?v=' + cache_version)
-
-                    $('head').append('<link rel="stylesheet" href="' + css + '">')
-
-                    putScript(
-                        urlJoin(lampa_url, 'lampainit.js?v=' + cache_version),
-                        false,
-                        function(){}
-                    )
-                },
-                loadLocalLampa
-            )
-        }
-    )
-}
-else{
-    loadCurrentLampa()
-}
+        putScript(urlJoin(lampa_url, `app.min.js?v=${cache_version}`), () => {
+            const css = urlJoin(lampa_url, `css/app.css?v=${cache_version}`);
+            $('head').append(`<link rel="stylesheet" href="${css}">`);
+        }, () => {
+            putScript(urlJoin(lampa_url, `lampa-main/app.min.js?v=${cache_version}`), () => {
+                const css = urlJoin(lampa_url, `lampa-main/css/app.css?v=${cache_version}`);
+                $('head').append(`<link rel="stylesheet" href="${css}">`);
+                putScript(urlJoin(lampa_url, `lampainit.js?v=${cache_version}`), false, () => {});
+            }, loadLocalLampa);
+        });
+    } else {
+        loadCurrentLampa();
+    }
+})();
