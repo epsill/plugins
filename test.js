2464e15cf0324d23b5b3397745e3f80d3af621de
epsill <164032791+epsill@users.noreply.github.com>
2025-04-28 12:34:30 +0500

    Update test.js

--- test.js
+++ test.js
@@ -2,16 +2,7 @@
     const serverProtocol = 'http://'; // Замените на нужный протокол (http или https)
     const iconServerRedirect = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
-            <g>
-                <path d="M13.4142 20.25 13 20.25V21.75ZM3.17157 19.8284L3.7019 19.2981H3.7019L3.17157 19.8284ZM20.8284 4.17157L20.2981 4.7019V4.7019L20.8284 4.17157ZM21.25 13C21.25 13.4142 21.5858 13.75 22 13.75C22.4142 13.75 22.75 13.4142 22.75 13H21.25ZM10 3.75H14V2.25H10V3.75ZM2.75 13V12H1.25V13H2.75ZM2.75 12V11H1.25V12H2.75ZM13 20.25H10V21.75H13V20.25ZM21.25 11V12H22.75V11H21.25ZM1.25 13C1.25 14.8644 1.24841 16.3382 1.40313 17.489C1.56076 18.6614 1.89288 19.6104 2.64124 20.3588L3.7019 19.2981C3.27869 18.8749 3.02502 18.2952 2.88976 17.2892C2...." fill="currentColor"></path>
-                <path d="M13 .5L18 .5" stroke="#ffffff" stroke-width="1" stroke-linecap="round"></path>
-                <path d="M6 .5L6 .5" stroke="#ffffff" stroke-width="1" stroke-linecap="round"></path>
-                <path d="M6 .5L6 .5" stroke="#ffffff" stroke-width="1" stroke-linecap="round"></path>
-                <path d="M9 .5L9 .5" stroke="#ffffff" stroke-width="1" stroke-linecap="round"></path>
-                <path d="M9 .5L9 .5" stroke="#ffffff" stroke-width="1" stroke-linecap="round"></path>
-                <path d="M15 .5H14V17Z"></path>
-                <!-- Добавьте остальные пути вашего SVG здесь -->
-            </g>
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm1-13h-2v6h2V7zm0 8h-2v2h2v-2z" fill="currentColor"/>
         </svg>`;
     
     function startMe() {
