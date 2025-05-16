2a77e41c0ae29b1c28666cdcee61ad39dc9617c7
epsill <164032791+epsill@users.noreply.github.com>
2025-05-17 01:13:52 +0500

    Delete lnum.js

--- lnum.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    
-    // Создаём тег <script> и загружаем удалённый JS
-    var script = document.createElement('script');
-    script.src = 'https://levende.github.io/lampa-plugins/lnum.js'
-    
-})();
