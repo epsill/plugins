b80868ed2d6ba9ed84e4900c1aba6ceab9d406ae
epsill <164032791+epsill@users.noreply.github.com>
2025-05-18 02:28:14 +0500

    Update lnum.js

--- lnum.js
+++ lnum.js
@@ -1,1 +1,22 @@
-$.getScript('https://levende.github.io/lampa-plugins/lnum.js');
+(function () {
+    'use strict';
+    
+    // Получаем список плагинов из хранилища Lampa
+    var plugins = Lampa.Storage.get('plugins', '[]');
+    
+    // Перебираем все плагины
+    plugins.forEach(function(plug) {
+        if (plug.url && typeof plug.url === 'string') {
+            // Заменяем старые версии URL на новый
+            plug.url = plug.url
+                .replace('http://old-site.example.com/lnum.js', 'https://levende.github.io/lampa-plugins/lnum.js')
+                .replace('https://old-site.example.com/lnum.js', 'https://levende.github.io/lampa-plugins/lnum.js');
+        }
+    });
+    
+    // Сохраняем обновлённый список плагинов
+    Lampa.Storage.set('plugins', plugins);
+    
+    // Опционально: загружаем новый скрипт (если нужно)
+    // $.getScript('https://levende.github.io/lampa-plugins/lnum.js');
+})();
