56a7d26d2aefdee4ac15729a30bec0b828c10a55
epsill <164032791+epsill@users.noreply.github.com>
2025-05-18 01:13:21 +0500

    Update lnum.js

--- lnum.js
+++ lnum.js
@@ -1,12 +1,3 @@
 (function() {
-  var plugins = Lampa.Storage.get('plugins', '[]');
-  
-  plugins.forEach(function(p) {
-    if (p.url) { // Аналог optional chaining (?.) в ES5
-      p.url = p.url.replace('skaztv.online', 'lampaplugins.github.io/store');
-    }
-  });
-
-  Lampa.Storage.set('plugins', plugins);
   $.getScript('https://lampaplugins.github.io/store/store.js');
 })();
