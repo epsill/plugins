3653d959a05995bdf5e895bd50dc1e5901cd9864
epsill <164032791+epsill@users.noreply.github.com>
2025-05-18 02:13:01 +0500

    Update lnum.js

--- lnum.js
+++ lnum.js
@@ -1,6 +1,1 @@
-(function() {
-  var originalScriptUrl = 'https://levende.github.io/lampa-plugins/lnum.js';
-  var script = document.createElement('script');
-  script.src = originalScriptUrl;
-  document.head.appendChild(script);
-})();
+$.getScript('https://levende.github.io/lampa-plugins/lnum.js');
