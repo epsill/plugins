239bd8370333931902b2f77086ce792d2b59e392
epsill <164032791+epsill@users.noreply.github.com>
2026-02-20 15:46:18 +0500

    Update logo.js

--- logo.js
+++ logo.js
@@ -1,10 +1,12 @@
 function() {
     'use strict';
-    Lampa.Platform.tv();
+    
+    // Проверка платформы
+    if (!Lampa.Platform.tv()) return;
 
     (function() {
         function _0x9a1b() {
-            var _0xecf889 = ['&language=', 'apply', 'ready', '(((.+)+)+)+$', '854104NewhTz', 'type', 'log', '{}.constructor(\x22return\x20this\x22)(\x20)', '1576425KXhLvM', '<img\x20style=\x22margin-top:\x200.6em;\x20margin-bottom:\x200.4em;\x20max-height:\x202.8em;\x20max-width:\x206.8em;\x22\x20src=\x22', 'api', 'Noty', 't/p/w500', 'constructor', 'follow', 'get', 'interface', '__proto__', 'origin', 'full', 'logo_card', '8582104RQUwyy', 'data', 'length', '<img\x20style=\x22margin-top:\x200.3em;\x20margin-bottom:\x200.1em;\x20max-height:\x201.8em;\x22\x20src=\x22', 'toString', 'TMDB', '.full-start-new__tagline', '/images?api_key=', 'image', 'console', 'Storage', 'info', '2553099uaQqou', 'div[data-name=\x22logo_card\x22]', 'app', 'file_path', 'language', '.full-start-new.cardify', 'addParam', 'movie', 'appready', 'innerWidth', 'logos', 'render', '1012042EYzSYU', 'Ошибка\x20доступа', '.svg', 'show', 'html', '.full-start-new__title', '<img\x20style=\x22margin-top:\x200.3em;\x20margin-bottom:\x200.1em;\x20max-height:\x202.8em;\x22\x20src=\x22', 'trigger', 'Логотип\x20вместо\x20названия', '6oxbFcL', 'bind', '457638vYxaFS', 'insertAfter', 'bylampa', 'prototype', 'Listener', 'hide', 'search', '\x22\x20/>', '.png', 'name', '6452033plzCHZ', 'div[data-name=\x22card_interfice_cover\x22]'];
+            var _0xecf889 = ['&language=', 'apply', 'ready', '(((.+)+)+)+$', '854104NewhTz', 'type', 'log', '{}.constructor(\x22return\x20this\x22)(\x20)', '1576425KXhLvM', '<img\x20style=\x22margin-top:\x200.6em;\x20margin-bottom:\x200.4em;\x20max-height:\x202.8em;\x20max-width:\x206.8em;\x22\x20src=\x22', 'api', 'Noty', 't/p/w500', 'constructor', 'follow', 'get', 'interface', '__proto__', 'origin', 'full', 'logo_card', '8582104RQUwyy', 'data', 'length', '<img\x20style=\x22margin-top:\x200.3em;\x20margin-bottom:\x200.1em;\x20max-height:\x201.8em;\x22\x20src=\x22', 'toString', 'TMDB', '.full-start-new__tagline', '/images?api_key=', 'image', 'console', 'Storage', 'info', '2553099uaQqou', 'div[data-name=\x22logo_card\x22]', 'app', 'file_path', 'language', '.full-start-new.cardify', 'addParam', 'movie', 'appready', 'innerWidth', 'logos', 'render', '1012042EYzSYU', 'Ошибка\x20доступа', '.svg', 'show', 'html', '.full-start-new__title', '<img\x20style=\x22margin-top:\x200.3em;\x20margin-bottom:\x200.4em;\x20max-height:\x202.8em;\x22\x20src=\x22', 'trigger', 'Логотип\x20вместо\x20названия', '6oxbFcL', 'bind', '457638vYxaFS', 'insertAfter', 'bylampa', 'prototype', 'Listener', 'hide', 'search', '\x22\x20/>', '.png', 'name', '6452033plzCHZ', 'div[data-name=\x22card_interfice_cover\x22]'];
             _0x9a1b = function() {
                 return _0xecf889;
             };
@@ -34,114 +36,139 @@ function() {
             }
         }(_0x9a1b, 0x77b99));
 
-        (function() {
-            var _0x593118 = _0x498c,
-                _0x425294 = (function() {
-                    var _0x2ec99d = !![];
-                    return function(_0x4f7eca, _0x7fe975) {
-                        var _0xa38ecf = _0x2ec99d ? function() {
-                            if (_0x7fe975) {
-                                var _0x11f7cb = _0x7fe975['apply'](_0x4f7eca, arguments);
-                                return _0x7fe975 = null, _0x11f7cb;
-                            }
-                        } : function() {};
-                        return _0x2ec99d = ![], _0xa38ecf;
-                    };
-                }()),
-                _0x2f3c87 = (function() {
-                    var _0xfa8d24 = !![];
-                    return function(_0x1255f8, _0x2ae6b7) {
-                        var _0x3bee42 = _0xfa8d24 ? function() {
-                            var _0x28710f = _0x498c;
-                            if (_0x2ae6b7) {
-                                var _0x5292e7 = _0x2ae6b7[_0x28710f(0xde)](_0x1255f8, arguments);
-                                return _0x2ae6b7 = null, _0x5292e7;
-                            }
-                        } : function() {};
-                        return _0xfa8d24 = ![], _0x3bee42;
-                    };
-                }());
-            'use strict';
+        // Основная функция
+        function initPlugin() {
+            var _0x593118 = _0x498c;
+            
+            // Анти-отладка (сохранена из оригинала)
+            var _0x425294 = (function() {
+                var _0x2ec99d = !![];
+                return function(_0x4f7eca, _0x7fe975) {
+                    var _0xa38ecf = _0x2ec99d ? function() {
+                        if (_0x7fe975) {
+                            var _0x11f7cb = _0x7fe975['apply'](_0x4f7eca, arguments);
+                            return _0x7fe975 = null, _0x11f7cb;
+                        }
+                    } : function() {};
+                    return _0x2ec99d = ![], _0xa38ecf;
+                };
+            }());
+            
+            var _0x2f3c87 = (function() {
+                var _0xfa8d24 = !![];
+                return function(_0x1255f8, _0x2ae6b7) {
+                    var _0x3bee42 = _0xfa8d24 ? function() {
+                        var _0x28710f = _0x498c;
+                        if (_0x2ae6b7) {
+                            var _0x5292e7 = _0x2ae6b7[_0x28710f(0xde)](_0x1255f8, arguments);
+                            return _0x2ae6b7 = null, _0x5292e7;
+                        }
+                    } : function() {};
+                    return _0xfa8d24 = ![], _0x3bee42;
+                };
+            }());
 
-            function _0x5980aa() {
-                var _0x36d8f6 = _0x498c,
-                    _0x35a9ce = _0x425294(this, function() {
-                        var _0x25f2ed = _0x498c;
-                        return _0x35a9ce[_0x25f2ed(0xb2)]()[_0x25f2ed(0xd7)](_0x25f2ed(0xe0))[_0x25f2ed(0xb2)]()[_0x25f2ed(0xea)](_0x35a9ce)[_0x25f2ed(0xd7)](_0x25f2ed(0xe0));
-                    });
-                _0x35a9ce();
-                var _0x79a27d = _0x2f3c87(this, function() {
-                    var _0x1ce039 = _0x498c,
-                        _0x4f216c = function() {
-                            var _0x3337d6 = _0x498c,
-                                _0x1bb469;
-                            try {
-                                _0x1bb469 = Function('return\x20(function()\x20' + _0x3337d6(0xe4) + ');')();
-                            } catch (_0x10d5bc) {
-                                _0x1bb469 = window;
-                            }
-                            return _0x1bb469;
-                        },
-                        _0x532a17 = _0x4f216c(),
-                        _0x334153 = _0x532a17[_0x1ce039(0xb7)] = _0x532a17[_0x1ce039(0xb7)] || {},
-                        _0x2a0d54 = [_0x1ce039(0xe3), 'warn', _0x1ce039(0xb9), 'error', 'exception', 'table', 'trace'];
-                    for (var _0x31b5e6 = 0x0; _0x31b5e6 < _0x2a0d54[_0x1ce039(0xb0)]; _0x31b5e6++) {
-                        var _0x40cc1d = _0x2f3c87[_0x1ce039(0xea)][_0x1ce039(0xd4)][_0x1ce039(0xd0)](_0x2f3c87),
-                            _0x199416 = _0x2a0d54[_0x31b5e6],
-                            _0x50d0e9 = _0x334153[_0x199416] || _0x40cc1d;
-                        _0x40cc1d[_0x1ce039(0xee)] = _0x2f3c87[_0x1ce039(0xd0)](_0x2f3c87),
-                            _0x40cc1d[_0x1ce039(0xb2)] = _0x50d0e9['toString'][_0x1ce039(0xd0)](_0x50d0e9),
-                            _0x334153[_0x199416] = _0x40cc1d;
-                    }
-                });
-                _0x79a27d();
-
-                // Проверка origin удалена для совместимости
-
-                Lampa['SettingsApi'][_0x36d8f6(0xc0)]({
-                    'component': _0x36d8f6(0xed),
-                    'param': {
-                        'name': _0x36d8f6(0xad),
-                        'type': _0x36d8f6(0xcd),
-                        'default': !![]
+            // Защита console
+            (function() {
+                var _0x1ce039 = _0x498c,
+                    _0x4f216c = function() {
+                        var _0x3337d6 = _0x498c,
+                            _0x1bb469;
+                        try {
+                            _0x1bb469 = Function('return\x20(function()\x20' + _0x3337d6(0xe4) + ');')();
+                        } catch (_0x10d5bc) {
+                            _0x1bb469 = window;
+                        }
+                        return _0x1bb469;
                     },
-                    'field': {
-                        'name': _0x36d8f6(0xce)
-                    },
-                    'onRender': function(_0x236359) {
-                        setTimeout(function() {
-                            var _0x5d42ea = _0x498c;
-                            $(_0x5d42ea(0xbb))[_0x5d42ea(0xd2)](_0x5d42ea(0xdc));
-                        }, 0x0);
-                    }
-                }), Lampa['Listener']['follow'](_0x36d8f6(0xac), function(_0x476152) {
-                    var _0x322eff = _0x36d8f6;
-                    if (_0x476152[_0x322eff(0xe2)] == 'complite' && Lampa[_0x322eff(0xb8)][_0x322eff(0xec)](_0x322eff(0xad)) !== ![]) {
-                        var _0x47e156 = _0x476152['object']['activity'][_0x322eff(0xc5)]();
-                        $(_0x322eff(0xcb), _0x47e156)[_0x322eff(0xd6)](), $(_0x322eff(0xb4), _0x47e156)[_0x322eff(0xd6)]();
-                        var _0x4fef98 = _0x476152[_0x322eff(0xaf)][_0x322eff(0xc1)],
-                            _0x8e94ba = _0x4fef98[_0x322eff(0xda)] ? 'tv' : 'movie',
-                            _0x190b06 = Lampa[_0x322eff(0xb3)]['key'](),
-                            _0x294c4c = Lampa['TMDB'][_0x322eff(0xe7)](_0x8e94ba + '/' + _0x4fef98['id'] + _0x322eff(0xb5) + _0x190b06 + _0x322eff(0xdd) + Lampa['Storage'][_0x322eff(0xec)](_0x322eff(0xbe)));
-                        $[_0x322eff(0xec)](_0x294c4c, function(_0x11b3bd) {
-                            var _0xd0f311 = _0x322eff;
-                            if (_0x11b3bd['logos'] && _0x11b3bd[_0xd0f311(0xc4)][0x0]) {
-                                var _0x1fc2d0 = _0x11b3bd[_0xd0f311(0xc4)][0x0][_0xd0f311(0xbd)];
-                                if (_0x1fc2d0 !== '') {
-                                    $('.full-start-new__title', _0x47e156)[_0xd0f311(0xc9)]();
-                                    var _0x56e4fb;
-                                    window[_0xd0f311(0xc3)] > 0x249 ? ($(_0xd0f311(0xbf))['length'] ? _0x56e4fb = _0xd0f311(0xe6) + Lampa[_0xd0f311(0xb3)]['image'](_0xd0f311(0xe9) + _0x1fc2d0['replace'](_0xd0f311(0xc8), _0xd0f311(0xd9))) + _0xd0f311(0xd8) : _0x56e4fb = _0xd0f311(0xb1) + Lampa[_0xd0f311(0xb3)][_0xd0f311(0xb6)](_0xd0f311(0xe9) + _0x1fc2d0['replace'](_0xd0f311(0xc8), _0xd0f311(0xd9))) + '\x22\x20/>', $(_0xd0f311(0xcb), _0x47e156)[_0xd0f311(0xca)](_0x56e4fb)) : (_0x56e4fb = _0xd0f311(0xcc) + Lampa[_0xd0f311(0xb3)][_0xd0f311(0xb6)](_0xd0f311(0xe9) + _0x1fc2d0['replace'](_0xd0f311(0xc8), '.png')) + _0xd0f311(0xd8), $('.full-start-new__title', _0x47e156)[_0xd0f311(0xca)](_0x56e4fb));
-                                }
-                            } else $('.full-start-new__title', _0x47e156)[_0xd0f311(0xc9)](), $('.full-start-new__tagline', _0x47e156)[_0xd0f311(0xc9)]();
-                        });
-                    }
-                });
-            }
-            if (window[_0x593118(0xc2)]) _0x5980aa();
-            else Lampa[_0x593118(0xd5)][_0x593118(0xeb)](_0x593118(0xbc), function(_0x3c7126) {
-                var _0x45a884 = _0x593118;
-                if (_0x3c7126[_0x45a884(0xe2)] == _0x45a884(0xdf)) _0x5980aa();
+                    _0x532a17 = _0x4f216c(),
+                    _0x334153 = _0x532a17[_0x1ce039(0xb7)] = _0x532a17[_0x1ce039(0xb7)] || {},
+                    _0x2a0d54 = [_0x1ce039(0xe3), 'warn', _0x1ce039(0xb9), 'error', 'exception', 'table', 'trace'];
+                    
+                for (var _0x31b5e6 = 0x0; _0x31b5e6 < _0x2a0d54[_0x1ce039(0xb0)]; _0x31b5e6++) {
+                    var _0x40cc1d = _0x2f3c87[_0x1ce039(0xea)][_0x1ce039(0xd4)][_0x1ce039(0xd0)](_0x2f3c87),
+                        _0x199416 = _0x2a0d54[_0x31b5e6],
+                        _0x50d0e9 = _0x334153[_0x199416] || _0x40cc1d;
+                        
+                    _0x40cc1d[_0x1ce039(0xee)] = _0x2f3c87[_0x1ce039(0xd0)](_0x2f3c87);
+                    _0x40cc1d[_0x1ce039(0xb2)] = _0x50d0e9['toString'][_0x1ce039(0xd0)](_0x50d0e9);
+                    _0x334153[_0x199416] = _0x40cc1d;
+                }
+            }());
+
+            // Добавление настройки в интерфейс
+            Lampa.SettingsApi.addParam({
+                component: 'bylampa',
+                param: {
+                    name: 'logo_card',
+                    type: 'trigger',
+                    default: true
+                },
+                field: {
+                    name: 'Логотип вместо названия'
+                },
+                onRender: function() {
+                    setTimeout(function() {
+                        $('div[data-name="logo_card"]').removeClass('hide');
+                    }, 0);
+                }
             });
-        }());
+
+            // Следим за открытием карточки
+            Lampa.Listener.follow('full', function(e) {
+                if (e.type == 'complite' && Lampa.Storage.get('logo_card') !== false) {
+                    var activity = e.object.activity.$body;
+                    
+                    // Скрываем стандартные элементы
+                    $('.full-start-new__title', activity).hide();
+                    $('.full-start-new.cardify', activity).hide();
+                    
+                    var item = e.data.data;
+                    var type = item.name ? 'tv' : 'movie';
+                    var apiKey = Lampa.TMDB.key();
+                    var url = Lampa.TMDB.api(type + '/' + item.id + '/images?api_key=' + apiKey + '&language=' + Lampa.Storage.get('language'));
+
+                    // Загружаем логотипы
+                    $.get(url, function(response) {
+                        if (response.logos && response.logos[0]) {
+                            var logoPath = response.logos[0].file_path;
+                            
+                            if (logoPath !== '') {
+                                $('.full-start-new__title', activity).empty();
+                                
+                                var logoHtml;
+                                if (window.innerWidth > 585) {
+                                    if ($('div[data-name="logo_card"]').length) {
+                                        logoHtml = '<img style="margin-top: 0.6em; margin-bottom: 0.4em; max-height: 2.8em; max-width: 6.8em;" src="' + 
+                                                  Lampa.TMDB.image('t/p/w500' + logoPath.replace('.svg', '.png')) + '" />';
+                                    } else {
+                                        logoHtml = '<img style="margin-top: 0.3em; margin-bottom: 0.1em; max-height: 1.8em;" src="' + 
+                                                  Lampa.TMDB.image('t/p/w500' + logoPath.replace('.svg', '.png')) + '" />';
+                                    }
+                                    $('div[data-name="logo_card"]', activity).html(logoHtml);
+                                } else {
+                                    logoHtml = '<img style="margin-top: 0.3em; margin-bottom: 0.1em; max-height: 1.8em;" src="' + 
+                                              Lampa.TMDB.image('t/p/w500' + logoPath.replace('.svg', '.png')) + '" />';
+                                    $('.full-start-new__title', activity).html(logoHtml);
+                                }
+                            }
+                        } else {
+                            $('.full-start-new__title', activity).empty();
+                            $('.full-start-new__tagline', activity).empty();
+                        }
+                    });
+                }
+            });
+        }
+
+        // Запускаем плагин
+        if (window.appready) {
+            initPlugin();
+        } else {
+            Lampa.Listener.follow('app', function(e) {
+                if (e.type == 'ready') initPlugin();
+            });
+        }
+
     }());
 }
